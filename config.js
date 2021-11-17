ef0d893b1eae0fbe0ca3765f9d67140630bd744d
Axdev <axdev2020@gmail.com>
2021-11-17 12:54:24 +0100

    first commit heroku

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 2019,
-  MONGODB_URL: process.env.MONGODB_URI || 'mongodb+srv://admin:admin4591@cluster0.t0x6x.mongodb.net/mendpressdb?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URI || 'mongodb+srv://admin:adminmdp@cluster0.t0x6x.mongodb.net/mendpressdb?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
   //PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   //accessKeyId: process.env.accessKeyId || 'accessKeyId',
