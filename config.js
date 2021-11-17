ba9aac56b9623bd0297bab7f7f36d7ce5a201864
Axdev <axdev2020@gmail.com>
2021-11-17 13:04:48 +0100

    first commit heroku1

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 2019,
-  MONGODB_URL: process.env.MONGODB_URI || 'mongodb+srv://admin:adminmdp@cluster0.t0x6x.mongodb.net/mendpressdb?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URI || 'mongodb+srv://admin:adminmdp@cluster0.mhofk.mongodb.net/test?authSource=admin&replicaSet=atlas-xioko1-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
   //PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   //accessKeyId: process.env.accessKeyId || 'accessKeyId',
